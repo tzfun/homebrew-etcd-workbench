240a260a4b1b00b51f977ca63ad84ceb36690ce8
dj <t@t.t>
2025-04-07 14:35:54 +0800

    Change version

--- Casks/etcd-workbench.rb
+++ Casks/etcd-workbench.rb
@@ -5,7 +5,7 @@ cask "etcd-workbench" do
   sha256 arm:   "578af510a50e8ff599578a3ef3f2c8d379368c56b5d60673e6512c9846383581",
          intel: "0470889204a7116938603d6938bcbbc0087888e91e78839c438d530484613942"
 
-  url "https://github.com/tzfun/etcd-workbench/releases/download/v#{version}/etcd-workbench-#{version}-macos-#{arch}.app.tar.gz",
+  url "https://github.com/tzfun/etcd-workbench/releases/download/App-#{version}/etcd-workbench-#{version}-macos-#{arch}.app.tar.gz",
       verified: "github.com/tzfun/etcd-workbench/"
   name "Etcd Workbench"
   desc "A powerful ui client for ETCD v3. Provides App and Web packages. Supports SSL and SSH Tunnel connections."
