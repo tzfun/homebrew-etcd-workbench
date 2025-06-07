4ff2a78f6f73c148f40f3579bd61fcccb14c9e82
Anka <runner@Mac-1749265026015.local>
2025-06-07 03:08:17 +0000

    etcd-workbench 1.1.7

--- Casks/etcd-workbench.rb
+++ Casks/etcd-workbench.rb
@@ -1,9 +1,9 @@
 cask "etcd-workbench" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.1.6"
-  sha256 arm:   "357970d4500602611c5ce5cbf954273d48f0a99f4440d07ead9f1b5f096afa6a",
-         intel: "0b1be450f9e90c0e115f2081ca34ede74f5abacf5fbfe68c01716ae16e43ebab"
+  version "1.1.7"
+  sha256 arm:   "43474e0dd4963bf5beff8aa1280d8725b231cd8d3651f2a45c97f41f6c91606c",
+         intel: "338ac87b565f623a55e56b5a94d690171c04d4debf0ff99bda28047b59f083ac"
 
   url "https://github.com/tzfun/etcd-workbench/releases/download/App-#{version}/etcd-workbench-#{version}-macos-#{arch}.app.tar.gz",
       verified: "github.com/tzfun/etcd-workbench/"
